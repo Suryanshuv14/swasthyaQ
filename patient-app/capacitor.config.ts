b683732a7ef9e32855cd834f44a77d60fa28af94
Suryanshu <vsuryanshu14@gmail.com>
2026-09-06 23:35:57 +0530

    add capcatior 1

---
+++ patient-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.swasthyaq.patient',
+  appName: 'SwasthyaQ',
+  webDir: 'out'
+};
+
+export default config;
